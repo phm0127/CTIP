1ba249dbc1a3193b2938e9131283c3e83b27ce1f
phm0127 <o_ogog@naver.com>
2019-04-26 08:46:53 +0900

    Update AppTest.java

--- src/test/java/CTIP/AppTest.java
+++ src/test/java/CTIP/AppTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
+        dfdfvz
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
